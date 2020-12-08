cda34c0400560cd69fca80d2c790d1540fc7d599
fpozzana <49064037+fpozzana@users.noreply.github.com>
2020-12-08 18:21:12 +0100

    Update tb_bw_multiplier.sv
    check for correctness against * directly in the testbench

--- Baugh-Wooley_multiplier/tb_bw_multiplier.sv
+++ Baugh-Wooley_multiplier/tb_bw_multiplier.sv
@@ -2,20 +2,21 @@
 
 module tb_bw_multiplier_s;
 
-parameter numBit = 4;
+parameter numBit = 8;
 
 logic [numBit-1:0] m_in, n_in;
 logic [2*numBit-1:0] o_out;
 
-bw_multiplier_s #(4) DUT (.m_in(m_in), .n_in(n_in), .o_out(o_out));
+bw_multiplier_s #(numBit) DUT (.m_in(m_in), .n_in(n_in), .o_out(o_out));
 
+/*
 initial
 begin
   m_in = 4'b0000;
   n_in = 4'b0001;
   #10us;
-  m_in = 4'b0101;
-  n_in = 4'b1010;
+  m_in = 4'b0111;
+  n_in = 4'b0111;
   #10us;
   m_in = 4'b0000;
   n_in = 4'b0001;
@@ -26,6 +27,30 @@ begin
   m_in = 4'b0011;
   n_in = 4'b0110;
   #10us;
+end
+*/
+integer i;
+integer j;
+
+initial begin
+  n_in = {numBit{1'b0}};
+  m_in = {numBit{1'b0}};
+  for(int i = 0; i < 2**numBit-1; i++)
+  begin
+    for(int i = 0; i < 2**numBit-1; i++)
+    begin
+      #10us;
+      //$display("Decimal converted to signed n %d", $signed(n_in));
+      //$display("Decimal converted to signed m %d", $signed(m_in));
+      //$display("Decimal converted to signed o %d", $signed(o_out));
+      if($signed(o_out) != $signed(n_in) * $signed(m_in))
+      begin
+        $display("Fail");
+      end
+      n_in = n_in + 1;
+    end
+    m_in = m_in + 1;
+  end
 end
 
 endmodule
